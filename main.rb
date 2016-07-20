9a27ed0810e2d9cf1cd05b374a05e0655d905699
Shuichiro Fukuoka <syumai@gmail.com>
2016-07-20 11:12:14 +0900

    Fixed indentation

--- main.rb
+++ main.rb
@@ -7,24 +7,24 @@ FCM_API_KEY = nil # Set your FCM API key
 subscription_data = JSON.parse(ARGV[0])
 
 def subscription_params subscription
-	{
-		endpoint: subscription[:endpoint],
-		p256dh: subscription[:keys][:p256dh],
-		auth: subscription[:keys][:auth],
-	}
+  {
+    endpoint: subscription[:endpoint],
+    p256dh: subscription[:keys][:p256dh],
+    auth: subscription[:keys][:auth],
+  }
 end
 
 message = {
-	title: "Example title",
-	body: "Example body",
-	url: "https://example.com"
+  title: "Example title",
+  body: "Example body",
+  url: "https://example.com"
 }
 
 Webpush.payload_send(
-	subscription_params(subscription_data).merge({
-		message: JSON.generate(message),
-		ttl: 600,
-		api_key: FCM_API_KEY || ENV['FCM_API_KEY']
-	})
+  subscription_params(subscription_data).merge({
+    message: JSON.generate(message),
+    ttl: 600,
+    api_key: FCM_API_KEY || ENV['FCM_API_KEY']
+  })
 )
 
